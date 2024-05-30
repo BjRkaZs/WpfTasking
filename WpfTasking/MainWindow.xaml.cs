5196c35e1b1d326c879fac81d9d1d1967382e970
BjRkaZs <153749085+BjRkaZs@users.noreply.github.com>
2024-05-31 00:24:46 +0200

    Update MainWindow.xaml.cs
    A School Paper-re kell kattintani ahhoz, hogy mozgatható legyen

--- WpfTasking/MainWindow.xaml.cs
+++ WpfTasking/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +23,30 @@ namespace WpfTasking
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Task1Button_Click(object sender, RoutedEventArgs e)
+        {
+            Pages.Task1 taskObj = new Pages.Task1();
+            showPage1.Children.Add(taskObj);
+        }
+        private void Task2Button_Click(object sender, RoutedEventArgs e)
+        {
+            Pages.Task2 taskObj = new Pages.Task2();
+            showPage2.Children.Add(taskObj);
+        }
+        private void Task3Button_Click(object sender, RoutedEventArgs e)
+        {
+            Pages.Task3 taskObj = new Pages.Task3();
+            showPage3.Children.Add(taskObj);
+        }
+        private void TextBlock_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
         }
 
     }
